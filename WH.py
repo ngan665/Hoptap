16e1bd39a75daee7cdd442c3066256e906bf317a
ngan665 <117029787+ngan665@users.noreply.github.com>
2022-11-13 23:03:57 +0700

    Update WH.py

--- WH.py
+++ WH.py
@@ -6,7 +6,6 @@
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 get_ipython().system('pip install openpyxl')
